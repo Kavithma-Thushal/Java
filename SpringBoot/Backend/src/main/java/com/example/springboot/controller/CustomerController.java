24af06f17279c5384fea6ff8a64a2065f94322b2
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-04-20 20:40:13 +0530

    Refactored 5

--- SpringBoot/Backend/src/main/java/com/example/springboot/controller/CustomerController.java
+++ SpringBoot/Backend/src/main/java/com/example/springboot/controller/CustomerController.java
@@ -5,9 +5,9 @@ import com.example.springboot.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/customer")
-@CrossOrigin
 public class CustomerController {
 
     @Autowired
--- SpringBoot/Backend/src/main/java/com/example/springboot/repository/CustomerRepository.java
+++ SpringBoot/Backend/src/main/java/com/example/springboot/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.example.springboot.repository;
 import com.example.springboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
