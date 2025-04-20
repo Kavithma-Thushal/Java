592a1686e8759c3cb9a3492ce5e90244ae63bc32
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-04-20 20:08:16 +0530

    Refactored 4

--- SpringBoot/Backend/src/main/java/com/example/springboot/controller/CustomerController.java
+++ SpringBoot/Backend/src/main/java/com/example/springboot/controller/CustomerController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/customers")
+@RequestMapping("api/v1/customer")
 @CrossOrigin
 public class CustomerController {
 
